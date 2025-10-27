392ba948c194c16badb2cbacea1b3ebc303d2852
Philip Ekalale <philipekalale07@gmail.com>
2025-10-27 10:47:06 +0300

    initial portfolio commit

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,16 +4,20 @@ export default function Projects() {
       <h2 className="text-3xl font-bold text-blue-400 mb-12 text-center">Projects</h2>
       <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
         <div className="bg-gray-800 p-6 rounded-xl shadow hover:shadow-lg transition">
-          <h3 className="text-xl font-semibold mb-2">BudgetEase</h3>
+          <h3 className="text-xl font-semibold mb-2 text-red-200">BudgetEase</h3>
           <p className="text-gray-400">A budget management app built using React, Express, and MongoDB.</p>
         </div>
         <div className="bg-gray-800 p-6 rounded-xl shadow hover:shadow-lg transition">
-          <h3 className="text-xl font-semibold mb-2">Meal Finder</h3>
+          <h3 className="text-xl font-semibold mb-2 text-red-200">Meal Finder</h3>
           <p className="text-gray-400">A recipe search app using the MealDB API built with React and Tailwind.</p>
         </div>
         <div className="bg-gray-800 p-6 rounded-xl shadow hover:shadow-lg transition">
-          <h3 className="text-xl font-semibold mb-2">E-commerce DB</h3>
+          <h3 className="text-xl font-semibold mb-2 text-red-200">E-commerce DB</h3>
           <p className="text-gray-400">A backend project showcasing relational database design in MySQL.</p>
+        </div>
+        <div className="bg-gray-800 p-6 rounded-xl shadow hover:shadow-lg transition">
+          <h3 className="text-xl font-semibold mb-2 text-red-200">Bandits Surveillance System</h3>
+          <p className="text-gray-400">A system that tracks and alert on criminals and bandits location ,time to attack,where and ways to mititgate the threat</p>
         </div>
       </div>
     </section>
