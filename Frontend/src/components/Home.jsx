f89cf4ea8e1f447a766332ae8b43f8543f10cba4
Philip Ekalale <philipekalale07@gmail.com>
2026-03-22 13:07:58 +0300

    updated image siz to fit mobile responsive

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -3,20 +3,20 @@ import Hero from "./Hero";
 
 function Home() {
   return (
-    <div className="flex flex-col min-h-screen p-15 ">
+    <div className="flex flex-col min-h-screen p-6 md:p-15">
       {/* Main Section */}
-      <div className="flex flex-col md:flex-row items-center justify-between flex-grow px-8 mt-10 space-x-6">
+      <div className="flex flex-col md:flex-row items-center justify-between flex-grow px-4 md:px-8 mt-10 md:space-x-6">
         {/* Left side - Hero content */}
         <div className="md:w-1/2 text-center md:text-left">
           <Hero />
         </div>
 
         {/* Right side - Image */}
-        <div className="md:w-1/2 flex justify-center md:justify-end mt-8 md:mt-0">
+        <div className="w-full md:w-1/2 flex justify-center md:justify-end mt-8 md:mt-0">
           <img
             src={myPhoto}
             alt="My portrait"
-            className="w-80 h-80 md:w-96 md:h-96 rounded-full shadow-lg"
+            className="w-[85vw] max-w-[22rem] aspect-square object-cover rounded-full shadow-lg md:w-96 md:h-96"
           />
         </div>
       </div>
